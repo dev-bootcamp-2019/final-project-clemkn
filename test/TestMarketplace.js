d2477e2aa3c7ca31623aa247011853a1c19c9ee3
clemkn <clemquen33@gmail.com>
2019-01-28 02:26:09 +0100

    Add comments to tests

--- test/TestMarketplace.js
+++ test/TestMarketplace.js
@@ -2,12 +2,23 @@ var Marketplace = artifacts.require("./Marketplace.sol");
 
 contract('Marketplace', function(accounts) {
 
+  /*
+   * This test ensures that the marketplace contract creator
+   * is correctly set as the first admin on the marketplace.
+   * This is important because only admins are allowed to
+   * take specific actions like add store owners from requests.
+   */
   it("should set an admin to the marketplace.", function() {
     return Marketplace.deployed()
       .then(instance => instance.admins.call(accounts[0]))
       .then(isAdmin => assert.equal(isAdmin, true, "The marketplace contract creator was not set as an admin."));
   });
 
+  /*
+   * This test ensures that an admin can add a new admin on the marketplace.
+   * This is important because only admins are allowed to
+   * take specific actions like add store owners from requests.
+   */
   it("should add an admin.", function() {
     let marketplaceInstance;
     return Marketplace.deployed().then(instance => {
@@ -18,6 +29,10 @@ contract('Marketplace', function(accounts) {
     .then(isAdmin => assert.equal(isAdmin, true, "The admin address was not stored correctly."));
   });
 
+  /*
+   * This test ensures that admins are able to
+   * convert users to store owners.
+   */
   it("should add a store owner.", function() {
     let marketplaceInstance;
     return Marketplace.deployed().then(instance => {
@@ -28,6 +43,10 @@ contract('Marketplace', function(accounts) {
     .then(isStoreOwner => assert.equal(isStoreOwner, true, "The store owner request address was not stored correctly."));
   });
 
+  /*
+   * This test ensures that a store owner can create a new
+   * store contract instance.
+   */
   it("should create a new store by the store owner.", function() {
     let marketplaceInstance;
     return Marketplace.deployed().then(instance => {
@@ -38,19 +57,31 @@ contract('Marketplace', function(accounts) {
     .then(stores => assert.equal(stores.length, 1, "Store was not created correctly."));
   });
 
-  it("should reconize address as an admin user.", function() {
+  /*
+   * This test ensures that the admin users are correctly identified
+   * in order to display the appropriate UI.
+   */
+  it("should recognize address as an admin user.", function() {
     return Marketplace.deployed()
       .then(instance => { return instance.getUserRole.call({from: accounts[0]}) })
       .then(userRole => assert.equal(userRole, "admin", "The address is not reconize as admin."));
   });
 
-  it("should reconize address as a store owner user.", function() {
+  /*
+   * This test ensures that the store owners are correctly identified
+   * in order to display the appropriate UI.
+   */
+  it("should recognize address as a store owner user.", function() {
     return Marketplace.deployed()
       .then(instance => { return instance.getUserRole.call({from: accounts[1]}) })
       .then(userRole => assert.equal(userRole, "storeOwner", "The address is not reconize as storeOwner."));
   });
 
-  it("should reconize address as a regular user.", function() {
+  /*
+   * This test ensures that the regular users are correctly identified
+   * in order to display the appropriate UI.
+   */
+  it("should recognize address as a regular user.", function() {
     return Marketplace.deployed()
       .then(instance => { return instance.getUserRole.call({from: accounts[2]}) })
       .then(userRole => assert.equal(userRole, "regular", "The address is not reconize as regular."));
--- test/TestStore.js
+++ test/TestStore.js
@@ -4,15 +4,27 @@ contract('Store', function(accounts) {
 
   let storeInstance;
 
+  /*
+   * We need to create a new instance of
+   * the store to perform the tests below.
+   */
   before(() => Store.new(accounts[1], 'Store name', 'Store description')
     .then(instance => storeInstance = instance)
   );
 
+  /*
+   * This test ensures that the store contract creator
+   * is correctly set as the owner of the store.
+   */
   it("should set the right owner.", function() {
     return storeInstance.owner.call()
       .then(owner => assert.equal(owner, accounts[1], "The store owner is not the right owner."));
   });
 
+  /*
+   * This test ensures that a store owner can create
+   * a new item in the store.
+   */
   it("should let the store owner create a new item", function() {
     return storeInstance.addNewItem.sendTransaction('Item name', 'Item description', 1, {from: accounts[1]})
       .then(() => storeInstance.itemsBySku.call(1))
@@ -21,19 +33,27 @@ contract('Store', function(accounts) {
       })
   });
 
-  it("should allow store owner to add inventory to an existing product.", function() {
+  /*
+   * This test ensures that the store owner can update
+   * the stock for an existing item.
+   */
+  it("should allow store owner to update stock to an existing product.", function() {
     return storeInstance.updateStockCount.sendTransaction(1, 3, {from: accounts[1]})
       .then(() => storeInstance.itemsBySku.call(1))
       .then(item => {
-        assert.equal(item[1].toNumber(), 3, "Product's inventory was not updated correctly.");
+        assert.equal(item[1].toNumber(), 3, "Item's stock was not updated correctly.");
       });
   });
 
+  /*
+   * This test ensures that the stock for an existing item
+   * is updated after a sale.
+   */
   it("should allow a shopper to purchase a product from the store, and product balance reduces by correct amount.", function() {
     return storeInstance.buyItem.sendTransaction(1, 3, {value: 3})
       .then(() => storeInstance.itemsBySku.call(1))
       .then(item => {
-        assert.equal(item[1].toNumber(), 0, "Product's inventory was not updated after a sale correctly.");
+        assert.equal(item[1].toNumber(), 0, "Item's stock was not updated after a sale correctly.");
       });
   });
 
